c2ca9b11d2d3083782ac74c1cb854e335f1bde2b
Henry Hernandez <henmois123@gmail.com>
2019-07-26 16:36:42 -0600

    Consuming API

---
+++ ApiWebNorthwind/Controllers/ProductClientController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApiWebNorthwind.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace ApiWebNorthwind.Controllers
+{
+    public class ProductClientController : Controller
+    {
+        string BaseUrl = "http://localhost:5846/api/";
+        public async Task<ActionResult> Index()
+        {
+            List<Product> prodInfo = new List<Product>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jason"));
+                /*** Call all the client 
+                                         ***/
+                HttpResponseMessage Res = await client.GetAsync("Product");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var ProdResponse = Res.Content.ReadAsStringAsync().Result;
+                    prodInfo = JsonConvert.DeserializeObject<List<Product>>(ProdResponse);
+
+                }
+                return View(prodInfo);
+                                       
+            }
+        }
+        // GET: ProductClient
+      /*  public ActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
---
+++ ApiWebNorthwind/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiWebNorthwind.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public String ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public String QuantityPerUnit { get; set; }
+        public float UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public Boolean Discontinued { get; set; }
+    }
+}
